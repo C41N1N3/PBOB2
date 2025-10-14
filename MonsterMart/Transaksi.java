9cf1f730c87e3a83c1e802707303707455ad2896
C41N1N3 <akhdanalwaafy@gmail.com>
2025-10-14 15:37:58 +0700

    Update Transaksi.java

--- MonsterMart/Transaksi.java
+++ MonsterMart/Transaksi.java
@@ -1,16 +1,14 @@
 public class Transaksi {
     String minuman;
-    String ukuran;
     int totalHarga;
 
-    public Transaksi(String minuman, String ukuran, int totalHarga) {
+    public Transaksi(String minuman, int totalHarga) {
         this.minuman = minuman;
-        this.ukuran = ukuran;
         this.totalHarga = totalHarga;
     }
 
     @Override
     public String toString() {
-        return minuman + " (" + ukuran + ") - Total: Rp" + totalHarga;
+        return minuman + " (Botolan) - Total: Rp" + totalHarga;
     }
 }
